6d8b9159a0e6f40beb91653c21e01147181a371d
meihaowei <meihawoei@aoe.com>
2020-08-28 20:02:42 +0800

    提交代码

--- FragmentDemo/app/src/main/java/com/test/fragmentdemo/MainActivity.java
+++ FragmentDemo/app/src/main/java/com/test/fragmentdemo/MainActivity.java
@@ -3,19 +3,25 @@ package com.test.fragmentdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.test.fragmentdemo.service.MyWallpaperService;
 
 import java.util.List;
 
 import static androidx.navigation.Navigation.findNavController;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_SET_LIVE_WALLPAPER = 152;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }).check();
+        findViewById(R.id.testBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+                intent.putExtra( WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                        new ComponentName(MainActivity.this, MyWallpaperService.class) );
+                startActivityForResult(intent, REQUEST_SET_LIVE_WALLPAPER);
+            }
+        });
     }
 
     //onSupportNavigateUp()方法的重写，意味着Activity将它的 back键点击事件的委托出去，如果当前并非栈中顶部的Fragment, 那么点击back键，返回上一个Fragment。
---
+++ FragmentDemo/app/src/main/java/com/test/fragmentdemo/service/MyWallpaperService.java
@@ -0,0 +1,14 @@
+package com.test.fragmentdemo.service;
+
+import android.service.wallpaper.WallpaperService;
+
+public class MyWallpaperService extends WallpaperService {
+
+    public MyWallpaperService() {
+    }
+
+    @Override
+    public Engine onCreateEngine() {
+        return null;
+    }
+}
